99288e05b23df1dddecc120e4413fdfac7e13855
TolotraGino <randriamahatratratolotragino@gmail.com>
2025-11-10 15:00:24 +0300

    sprint1bis

---
+++ sprint1Bis/src/com/example/servlet/FrontServlet.java
@@ -0,0 +1,47 @@
+package com.example.servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
+
+@WebServlet("/")
+public class FrontServlet extends HttpServlet {
+
+    RequestDispatcher defaultDispatcher;
+
+    @Override
+    public void init() {
+        defaultDispatcher = getServletContext().getNamedDispatcher("default");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+
+        String path = req.getRequestURI().substring(req.getContextPath().length());
+        boolean resourceExists = getServletContext().getResource(path) != null;
+
+        if (resourceExists) {
+            defaultServe(req, res);
+        } else {
+            customServe(req, res);
+        }
+    }
+
+    private void customServe(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        res.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = res.getWriter()) {
+            String url = req.getRequestURI();
+            out.println("<html><head><title>FrontServlet</title></head><body>");
+            out.println("<h1>URL demandée : " + url + "</h1>");
+            out.println("<p>Ceci est le FrontServlet. Aucune ressource trouvée pour cette URL.</p>");
+            out.println("</body></html>");
+        }
+    }
+
+    private void defaultServe(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        defaultDispatcher.forward(req, res);
+    }
+}
