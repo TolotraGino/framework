d74a6f3e6b9d5c3eefbf2e3a957c0c235a76a2e9
TolotraGino <randriamahatratratolotragino@gmail.com>
2025-11-10 15:06:49 +0300

    sprint2bis

---
+++ Sprint2-bis/services/Test1.java
@@ -0,0 +1,10 @@
+package services;
+
+import annotations.ScanAnnotation;
+
+@ScanAnnotation
+public class Test1 {
+    public void execute() {
+        System.out.println("👋 Hello from HelloService");
+    }
+}
