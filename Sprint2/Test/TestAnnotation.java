5fc90e0c38cc823067e2fe2bb7125050dadee302
TolotraGino <randriamahatratratolotragino@gmail.com>
2025-12-08 15:30:56 +0300

    main

--- Sprint2/Test/TestAnnotation.java
+++
@@ -1,6 +0,0 @@
-public class TestAnnotation {
-    public static void main(String[] args) {
-        // Appel simple à l'utilitaire pour vérifier la classe exemple
-        AnnotationChecker.checkAnnotations(AnnotatedClass.class);
-    }
-}
